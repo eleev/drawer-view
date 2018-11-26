b7737aa5aa42d5cde7ba4c775c830f008126c8a7
Astemir Eleev <astemireleev@gmail.com>
2018-11-26 17:16:29 +0300

    Added new target called `demo`

---
+++ demo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  demo
+//
+//  Created by Astemir Eleev on 26/11/2018.
+//  Copyright © 2018 Astemir Eleev. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
