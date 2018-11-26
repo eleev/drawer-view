47d48d13ff6ed729ddbef4e0851a917d8f064915
Astemir Eleev <astemireleev@gmail.com>
2018-11-26 17:12:31 +0300

    Initial Commit

---
+++ drawer-view/drawer_view.h
@@ -0,0 +1,19 @@
+//
+//  drawer_view.h
+//  drawer-view
+//
+//  Created by Astemir Eleev on 26/11/2018.
+//  Copyright © 2018 Astemir Eleev. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for drawer_view.
+FOUNDATION_EXPORT double drawer_viewVersionNumber;
+
+//! Project version string for drawer_view.
+FOUNDATION_EXPORT const unsigned char drawer_viewVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <drawer_view/PublicHeader.h>
+
+
